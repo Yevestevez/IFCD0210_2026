a013971b302d54723ee34e074522ed0d525d1ba5
Alejandro Cerezo <alce65@hotmail.es>
2026-01-19 12:02:49 +0100

    Add sample1 datos

---
+++ 2.Pre.JS/in-situ/sample1-datos.js
@@ -0,0 +1,32 @@
+// Primitivos (7)
+console.log('Hola Mundo 22 😊 !! true'); // string
+console.log(typeof 'Hola Mundo 22 😊 !! true'); // string
+console.log(4) // number
+console.log(typeof 4) // number
+console.log(4.5) // number
+console.log(typeof 4) // number
+
+console.log(true) // boolean
+console.log(typeof true) // boolean
+console.log(false) // boolean
+console.log(typeof true) // boolean
+//
+console.log(undefined) // undefined
+console.log(typeof undefined) // undefined
+console.log(null) // ¿null?
+console.log(typeof null) // ¿object?
+//
+console.log(5n) // bigint
+console.log(typeof 5n) // bigint
+console.log(Symbol()) // symbol
+console.log(typeof Symbol()) // symbol
+
+// Referenciados u objetos (1)
+
+console.log({}) // object
+console.log(typeof {}) // object
+console.log([]) // object
+console.log(typeof []) // object
+
+console.log(() => {}) // function (object)
+console.log(typeof(() => {})) // function
